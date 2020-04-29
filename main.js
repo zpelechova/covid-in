8ad60edbef9bf1867362509847457381a55d32a8
Zuzana Pelechová <zpelechova@gmail.com>
2020-04-29 08:04:00 +0200

    added regions

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ let check = false;
 
 Apify.main(async () => {
 
-    const kvStore = await Apify.openKeyValueStore('COVID-19-IN');
-    const dataset = await Apify.openDataset('COVID-19-IN-HISTORY');
-    const { email } = await Apify.getValue('INPUT');
+    // const kvStore = await Apify.openKeyValueStore('COVID-19-IN');
+    // const dataset = await Apify.openDataset('COVID-19-IN-HISTORY');
+    // const { email } = await Apify.getValue('INPUT');
 
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
@@ -27,7 +27,15 @@ Apify.main(async () => {
         const activeCases = $('#site-dashboard > div > div > div > div > ul > li.bg-blue > strong').text();
         const recovered = $("#site-dashboard > div > div > div > div > ul > li.bg-green > strong").text();
         const deaths = $('#site-dashboard > div > div > div > div > ul > li.bg-red > strong').text();
-        
+
+        const regionsTableRows = Array.from(document.querySelectorAll("#state-data > div > div > div > div > table > tbody > tr"));
+        const regionData = [];
+
+        for (const row of regionsTableRows) {
+            const cells = Array.from(row.querySelectorAll("td")).map(td => td.textContent);
+            regionData.push({region: cells[1], totalInfected: cells[2], recovered: cells[3], deceased: cells[4] });
+        }
+
         const data = {
             activeCases: activeCases,
             recovered: recovered,
@@ -36,6 +44,7 @@ Apify.main(async () => {
             sourceUrl: 'https://www.mohfw.gov.in/',
             lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
             readMe: 'https://github.com/zpelechova/covid-in/blob/master/README.md',
+            regionData: regionData
         };
         return data;
 
@@ -43,45 +52,45 @@ Apify.main(async () => {
 
     console.log(result)
 
-    if (!result.activeCases || !result.deaths || !result.recovered) {
-        check = true;
-    }
-    else {
-        let latest = await kvStore.getValue(LATEST);
-        if (!latest) {
-            await kvStore.setValue('LATEST', result);
-            latest = result;
-        }
-        delete latest.lastUpdatedAtApify;
-        const actual = Object.assign({}, result);
-        delete actual.lastUpdatedAtApify;
+    // if (!result.activeCases || !result.deaths || !result.recovered) {
+    //     check = true;
+    // }
+    // else {
+    //     let latest = await kvStore.getValue(LATEST);
+    //     if (!latest) {
+    //         await kvStore.setValue('LATEST', result);
+    //         latest = result;
+    //     }
+    //     delete latest.lastUpdatedAtApify;
+    //     const actual = Object.assign({}, result);
+    //     delete actual.lastUpdatedAtApify;
 
-        if (JSON.stringify(latest) !== JSON.stringify(actual)) {
-            await dataset.pushData(result);
-        }
+    //     if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+    //         await dataset.pushData(result);
+    //     }
 
-        await kvStore.setValue('LATEST', result);
-        await Apify.pushData(result);
-    }
+    //     await kvStore.setValue('LATEST', result);
+    //     await Apify.pushData(result);
+    // }
 
 
-    console.log('Closing Puppeteer...');
-    await browser.close();
-    console.log('Done.');
+    // console.log('Closing Puppeteer...');
+    // await browser.close();
+    // console.log('Done.');
 
-    // if there are no data for activeCases etc., send email, because that means something is wrong
-    const env = await Apify.getEnv();
-    if (check) {
-        await Apify.call(
-            'apify/send-mail',
-            {
-                to: email,
-                subject: `Covid-19 IN from ${env.startedAt} failed `,
-                html: `Hi, ${'<br/>'}
-                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-                        run had 0 TotalInfected, check it out.`,
-            },
-            { waitSecs: 0 },
-        );
-    };
+    // // if there are no data for activeCases etc., send email, because that means something is wrong
+    // const env = await Apify.getEnv();
+    // if (check) {
+    //     await Apify.call(
+    //         'apify/send-mail',
+    //         {
+    //             to: email,
+    //             subject: `Covid-19 IN from ${env.startedAt} failed `,
+    //             html: `Hi, ${'<br/>'}
+    //                     <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
+    //                     run had 0 TotalInfected, check it out.`,
+    //         },
+    //         { waitSecs: 0 },
+    //     );
+    // };
 });
